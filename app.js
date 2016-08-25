f0537b02d1562f80ed752ade69d8cb83695aa695
urnmo <aaron0moore@gmail.com>
2016-08-24 22:12:12 -0400

    struggling

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 function getScramble() {
-//     // let request = new XMLHttpRequest();
-//     // request.open("GET", 'https://harold-ejcpxaenph.now.sh/scrambled');
-//     // request.addEventListener('load', function () {
-//     //     let response = JSON.parse(request.responseText);
-//     //     let word = response.scrambled;
-//     //     console.log(word);
+    let request = new XMLHttpRequest();
+    request.open("GET", 'https://harold-ejcpxaenph.now.sh/scrambled');
+    request.addEventListener('load', function () {
+        let response = JSON.parse(request.responseText);
+        let word = response.scrambled;
+        console.log(word);
+        let letters = word.split('');
+        letters.push = letter;
 
-
-
-//     })
+    })
 
 //define the parent
-window.addEventListener('load', function(){
     let parent = document.querySelector('#version');
     let template = document.querySelector('#scramble-template').innerHTML;
     let scrambles = [];
 
     scrambles.push({
         title: 'UnsCRAMble IT',
-        letter: [f, a, r, t,],
+        letter: [],
         correct: "you got it bucko",
         incorrect: "Wrong, sporto",
         });
@@ -27,10 +26,10 @@ for (let i = 0; i < scrambles.length; i++){
     let section = document.createElement('section');
    section.innerHTML = Mustache.render(template, scrambles[i]);
    parent.appendChild(section);
-    }   
+    } 
      request.send();
      console.log(scrambles);
-})
+
 }
 
 
