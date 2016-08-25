1e9f437eaabe4ae51e70e96e8049bf0784743548
urnmo <aaron0moore@gmail.com>
2016-08-24 20:11:48 -0400

    items to add staged but not functioning

--- public/app.js
+++ public/app.js
@@ -1,18 +1,41 @@
-function getScramble(){
-    let request = new XMLHttpRequest();
-    request.open("GET",  'https://harold-ejcpxaenph.now.sh/scrambled');
-    request.addEventListener('load', function(){
-        let response = JSON.parse(request.responseText);
-        let word = response.scrambled;
-        console.log(word);
-    })
-    request.send();
+function getScramble() {
+//     // let request = new XMLHttpRequest();
+//     // request.open("GET", 'https://harold-ejcpxaenph.now.sh/scrambled');
+//     // request.addEventListener('load', function () {
+//     //     let response = JSON.parse(request.responseText);
+//     //     let word = response.scrambled;
+//     //     console.log(word);
+
+
+
+//     })
+
+//define the parent
+window.addEventListener('load', function(){
+    let parent = document.querySelector('#version');
+    let template = document.querySelector('#scramble-template').innerHTML;
+    let scrambles = [];
+
+    scrambles.push({
+        title: 'UnsCRAMble IT',
+        letter: [f, a, r, t,],
+        correct: "you got it bucko",
+        incorrect: "Wrong, sporto",
+        });
+
+for (let i = 0; i < scrambles.length; i++){
+    let section = document.createElement('section');
+   section.innerHTML = Mustache.render(template, scrambles[i]);
+   parent.appendChild(section);
+    }   
+     request.send();
+     console.log(scrambles);
+})
 }
-    function refresh(){
 
-        
-    }
 
-       window.addEventListener('load', function(){
-            getScramble();
-        })
+
+window.addEventListener('load', function () {
+    getScramble();
+})
+
